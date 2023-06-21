302bc9478339f4e802936f842d81041639395b0f
fsponton <francisco.sponton@gmail.com>
2023-06-21 18:36:18 -0300

    changes

--- api-services/src/controllers/index.js
+++ api-services/src/controllers/index.js
@@ -1,7 +1,6 @@
 const { catchedAsync } = require("../utils")
 
 module.exports = {
-    validateMovie: require("../helpers/checkNulls"),
     getList: catchedAsync(require("./getList")),
     createObject: catchedAsync(require("./createObject")),
     getById: catchedAsync(require("./getById")),
--- database/src/helpers/index.js
+++ database/src/helpers/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     stringToArray: require("./stringToArray"),
     checkAndModifyName: require("./checkAndModifyName"),
-    validateMovie: require("../../../Api-services/src/helpers/checkNulls"),
     firstUpper: require("./firstUpper")
 }
