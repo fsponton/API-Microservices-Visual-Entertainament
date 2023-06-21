40ac176c008227efef9be9d44fa44924a444d6ce
fsponton <francisco.sponton@gmail.com>
2023-06-21 18:13:28 -0300

    some changes

--- Api-services/src/config/enviroments.js
+++ Api-services/src/config/enviroments.js
@@ -2,7 +2,7 @@ require("dotenv").config({ path: "././.env" })
 const { stringToArray } = require("../helpers")
 
 module.exports = {
-    PORT: 8002,
+    PORT: process.env.PORT,
     PASSWORD_SIGN: process.env.PASSWORD_SIGN,
     ENTITIES: stringToArray(process.env.ENTITIES)
 }   
