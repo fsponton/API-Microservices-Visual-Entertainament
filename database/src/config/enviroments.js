fe8d8b66d079b6b066bc4b5bac09778a5d41e817
fsponton <francisco.sponton@gmail.com>
2023-06-22 14:25:35 -0300

    repair number of ports

--- database/src/config/enviroments.js
+++ database/src/config/enviroments.js
@@ -3,7 +3,7 @@ const { stringToArray } = require("../helpers")
 
 module.exports = {
     URI_CONNECTION: process.env.URI_CONNECTION,
-    PORT: 8000,
+    PORT: process.env.PORT,
     PASSWORD_SIGN: process.env.PASSWORD_SIGN,
     ENTITIES: stringToArray(process.env.ENTITIES)
 }   
